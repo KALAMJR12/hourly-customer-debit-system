3940540441a1b27a308b7951063e177c16f207b9
Musa Umar Musa <123275895+KALAMJR12@users.noreply.github.com>
2025-08-03 10:56:31 +0100

    Update db.js

--- db.js
+++ db.js
@@ -5,7 +5,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const db = postgres(process.env.DATABASE_URL, {
-  ssl: 'require', // Required for Supabase-hosted PostgreSQL
+  ssl: 'require'
 });
 
 export default db;
