3f2b8c095cce3466f0e08dcc2b99e66d999ea3f2
Musa Umar Musa <123275895+KALAMJR12@users.noreply.github.com>
2025-08-03 10:44:41 +0100

    Update server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const debitProcessorRoutes = require('./api/debit-processor.js');
 const scheduler = require('./scheduler');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors());
